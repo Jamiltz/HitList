9cf20c93e59c744cfc52280067fcfded51dcc389
James Nocentini <james.nocentini@gmail.com>
2015-02-19 16:04:48 +0000

    tmp work

--- HitList/CoreDataStack.swift
+++ HitList/CoreDataStack.swift
@@ -13,13 +13,13 @@ class CoreDataStack {
     let context:NSManagedObjectContext
     let psc:NSPersistentStoreCoordinator
     let model:NSManagedObjectModel
-    let store:NSPersistentStore?
+    let store:CBLIncrementalStore?
     
     init() {
         //1
         let bundle = NSBundle.mainBundle()
         let modelURL = bundle.URLForResource("HitList", withExtension:"momd")
-        model = NSManagedObjectModel(contentsOfURL: modelURL!)!
+        model = NSManagedObjectModel.mergedModelFromBundles(nil)!.mutableCopy() as NSManagedObjectModel
         
         //1.1
         CBLIncrementalStore.updateManagedObjectModel(model)
@@ -33,30 +33,38 @@ class CoreDataStack {
         
         //4
         let documentsURL = applicationDocumentsDirectory()
-        let storeURL = documentsURL.URLByAppendingPathComponent("HitList")
+        let defaultStoreURL = documentsURL.URLByAppendingPathComponent("HitList")
         
-        let options = [NSMigratePersistentStoresAutomaticallyOption: true]
-        
-        
-        let databaseName = "String"
-        if !(CBLManager.sharedInstance().existingDatabaseNamed(databaseName, error: nil) != nil) {
-            let importStore = psc.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: storeURL, options: options, error: nil)
-            store = psc.migratePersistentStore(importStore, toURL: <#NSURL#>, options: <#[NSObject : AnyObject]?#>, withType: <#String#>, error: <#NSErrorPointer#>)
-        }
-        
+        let options: NSDictionary = [NSMigratePersistentStoresAutomaticallyOption: true, NSInferMappingModelAutomaticallyOption: true]
+        let databaseName = "hitlist"
+        let storeURL = NSURL(string: databaseName)!
         var error: NSError? = nil
-        store = psc.addPersistentStoreWithType(CBLIncrementalStore.type(), configuration: nil, URL: storeURL, options: options, error: &error)
+        
+        if (!(CBLManager.sharedInstance().existingDatabaseNamed(databaseName, error: nil) != nil)) {
+//            let defaultStoreURL = bundle.URLForResource("HitList", withExtension: "sqlite")
+            let importStore = psc.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: defaultStoreURL, options: options, error: nil)
+            store = psc.migratePersistentStore(importStore!, toURL: storeURL, options: options, withType: CBLIncrementalStore.type(), error: nil) as CBLIncrementalStore
+        } else {
+            store = psc.addPersistentStoreWithType(CBLIncrementalStore.type(), configuration: nil, URL: storeURL, options: options, error: &error) as CBLIncrementalStore
+        }
         
 //        store = psc.addPersistentStoreWithType(NSSQLiteStoreType,
 //            configuration: nil,
 //            URL: storeURL,
-//            options: options,
+//           options: options,
 //            error:&error)
 //        
         if store == nil {
             println("Error adding persistent store: \(error)")
             abort()
         }
+        
+        let url =  NSURL(string: "http://178.62.81.153:4984/hitlist/")
+        let pull = store?.database.createPullReplication(url)
+        let push = store?.database.createPushReplication(url)
+        
+        pull?.start()
+        push?.start()
         
     }
     
